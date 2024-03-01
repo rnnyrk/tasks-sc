e645905cb5461daa1d094e210992af428911a5e4
Ronny Rook <info@ronnyrook.nl>
2024-03-01 14:10:36 +0100

    Don't submit empty form

--- src/components/modules/tasks/AddTaskForm.tsx
+++ src/components/modules/tasks/AddTaskForm.tsx
@@ -22,6 +22,7 @@ export function AddTaskForm() {
   const { mutateAsync: onCreatePost } = useCreateTask();
 
   async function onSubmit(values: AddTaskFormType) {
+    if (!values.title) return;
     await onCreatePost(values);
     form.reset();
   }
